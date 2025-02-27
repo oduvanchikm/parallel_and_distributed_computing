4606f00d277bd09f3daafa3791e1ae0a92c41b7b
Mira <141563756+oduvanchikm@users.noreply.github.com>
2025-02-27 15:28:46 +0400

    Update lab1.cu

--- lab1.cu
+++ lab1.cu
@@ -4,7 +4,7 @@
 
 #define SIZE_BLOCK 512
 
-__global__ void reverse_array_function(float* input_array, float* output_arra, int n)
+__global__ void reverse_array_function(double* input_array, double* output_arra, int n)
 {
     for (int idx = blockIdx.x * blockDim.x + threadIdx.x; idx < n; idx += gridDim.x * blockDim.x)
     {
@@ -17,8 +17,8 @@ int main()
     int n;
     scanf("%d", &n);
 
-    float* input_array = (float*)malloc(sizeof(float) * n);
-    float* reverse_array = (float*)malloc(sizeof(float) * n);
+    double* input_array = (double*)malloc(sizeof(double) * n);
+    double* reverse_array = (double*)malloc(sizeof(double) * n);
     if (!input_array || !reverse_array)
     {
         printf("Allocation error in malloc\n");
@@ -30,10 +30,10 @@ int main()
         scanf("%f", &input_array[i]);
     }
 
-    float* input_array_cuda;
-    float* reverse_array_cuda;
+    double* input_array_cuda;
+    double* reverse_array_cuda;
 
-    cudaError_t error_1 = cudaMalloc(&input_array_cuda, sizeof(float)* n);
+    cudaError_t error_1 = cudaMalloc(&input_array_cuda, sizeof(double)* n);
     if (error_1 != cudaSuccess)
     {
       printf("cudaMalloc1 failed: %s\n", cudaGetErrorString(error_1));
@@ -41,14 +41,14 @@ int main()
     }
 
 
-    cudaError_t error_2 = cudaMalloc(&reverse_array_cuda,sizeof(float)* n);
+    cudaError_t error_2 = cudaMalloc(&reverse_array_cuda,sizeof(double)* n);
     if (error_2 != cudaSuccess)
     {
       printf("Allocation error in cudaMalloc2");
       return 1;
     }
 
-    cudaError_t error_3 = cudaMemcpy(input_array_cuda, input_array, n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaError_t error_3 = cudaMemcpy(input_array_cuda, input_array, n * sizeof(double), cudaMemcpyHostToDevice);
     if (error_3 != cudaSuccess)
     {
       printf("Allocation error in cudaMemcpy1");
@@ -57,7 +57,7 @@ int main()
 
     reverse_array_function<<<SIZE_BLOCK, SIZE_BLOCK>>>(input_array_cuda, reverse_array_cuda, n);
 
-    cudaError_t error_4 = cudaMemcpy(reverse_array, reverse_array_cuda,sizeof(float)* n, cudaMemcpyDeviceToHost);
+    cudaError_t error_4 = cudaMemcpy(reverse_array, reverse_array_cuda,sizeof(double)* n, cudaMemcpyDeviceToHost);
     if (error_4 != cudaSuccess)
     {
       printf("Allocation error in cudaMemcpy2");
